81198c5f83537a614b87be3164252c23755aadb9
Rusty <imkhazix@gmail.com>
2022-05-28 03:10:18 +0530

    calendar pending

--- calendar-maker/calendar.py
+++ calendar-maker/calendar.py
@@ -1,1 +1,17 @@
+"""
+Project 08
+Calendar Maker
+
+Author: Rusty
+GitHub: https://github.com/rustyxlol/TBBoSPP
+"""
+
 # TODO
+
+
+def generate_calendar(date):
+    pass
+
+
+if __name__ == "__main__":
+    pass
